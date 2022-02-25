6a205eaa48f7d8ef99fb858c45c7651cd6563909
TobiasBruenger <tbrueng1@uni-koeln.de>
2022-02-25 01:39:50 +0100

    Added interactive plots - first steps
    interactive plots of sign up, appointment call, visit, and contact were added, They can be filktered by all listed criteria besides "vlaue" or "log" so far. So basic pie chart Demographics for "sign up" are available. Could be expanded to the others as well

--- app.R
+++ app.R
@@ -3,10 +3,134 @@ library(shiny)
 library(shinydashboard)
 library(readr)
 library(shinyWidgets)
-library(shinyThings)  #devtools::install_github("gadenbuie/shinyThings")
+library(shinyThings)  
+library(tidyverse)
+library(plotly)
+library(lubridate)
+#devtools::install_github("gadenbuie/shinyThings")
 
 
 data <- read_csv("sample_data_dashboard.csv")
+
+end_date <- as.Date("2022-12-31") ##set a time in the future to account for visits that are sheduled for the future 
+current_day <- Sys.time()
+  
+#data modification 
+data_mod.df <- data %>% 
+  mutate(sign_up_datetime = gsub(" .*","",sign_up_datetime) %>% as.Date(),
+         initial_contact_date = gsub(" .*","",initial_contact_date) %>% as.Date(),
+         appointment_call_date = gsub(" .*","",appointment_call_date) %>% as.Date(),
+         visit_schedule_date = gsub(" .*","",visit_schedule_date) %>% as.Date()) %>% 
+  mutate(race_combine = ifelse(is.na(race_combine),"Unknown/ not disclosed",race_combine),
+         sex_combine = ifelse(is.na(sex_combine),"Unknown/ not disclosed",sex_combine))
+
+
+#predefined plot functions 
+overviewplot_1 <- function(var,xaxis,date_sel,age_filter,date_filter,race_filter,gender_filter){
+  
+  print(age_filter)
+  
+  start_date = as.Date(date_filter[1])
+  end_date = as.Date(date_filter[2])
+  
+  data_filt.df <- data_mod.df %>% 
+    rename(selected_var = var) %>% 
+    filter(age_combine >= age_filter[1] & age_combine <= age_filter[2],
+           selected_var >= date_filter[1] & selected_var <= date_filter[2],
+           race_combine %in% race_filter,
+           sex_combine %in% gender_filter) %>% 
+    group_by(selected_var) %>% 
+    summarise(n = n())
+  
+  ##if data should be aggregated per week 
+  if(date_sel == "Per week"){
+    data_filt.df <- data_filt.df %>% 
+      mutate(year = gsub("-.*","",selected_var),
+             week = week(selected_var),
+             Dates = paste0(year,"-",week)) %>% 
+      group_by(Dates) %>% 
+      summarise(n = sum(n,na.rm = T)) %>% 
+      filter(!is.na(Dates))
+    
+    date_template.df <- tibble(Dates = seq(start_date,end_date, by = "days")) %>% 
+      mutate(year = gsub("-.*","",Dates),
+             week = week(Dates),
+             Dates = paste0(year,"-",week)) %>% 
+      distinct(Dates)
+    
+    
+  } else{ ##per days view 
+    
+    date_template.df <- tibble(Dates = seq(start_date,end_date, by = "days"))
+    
+  }
+  
+  
+  date_input.df <- date_template.df %>% 
+    left_join(data_filt.df %>% setNames(c("Dates","count"))) %>% 
+    replace(is.na(.),0)
+  
+  
+  if(date_sel == "Cumulative"){
+    
+    date_input.df <- date_input.df %>% 
+      mutate(cumu_count = cumsum(count))
+    
+    py <- plot_ly(data = date_input.df,
+                  type = 'scatter', 
+                  mode = 'lines',
+                  x = ~Dates,
+                  y = ~cumu_count, 
+                  fill = 'tozeroy') %>% 
+      layout(yaxis = list(title ="Number of Sign-ups"),
+             xaxis = list(title = ""))
+    
+    
+  } else{
+    
+    py <- plot_ly(data = date_input.df,
+                  type = "bar",
+                  x = ~Dates,
+                  y = ~count) %>% 
+      layout(yaxis = list(title ="Number of Sign-ups"),
+             xaxis = list(title = ""))
+    
+  }
+  
+  return(py)
+  
+}
+
+demographics_pie <- function(var_demo,var_date,title_sel,age_filter,date_filter,race_filter,gender_filter){
+  
+  pie_input.df <- data_mod.df %>% 
+    ##duplicate to avoid issues when we rename certain variables 
+    mutate(age_combine1 = age_combine,
+           race_combine1 = race_combine,
+           sex_combine1 = sex_combine) %>% 
+    rename(selected_var_date = var_date,
+           selected_var_demo = var_demo) %>% 
+    filter(age_combine1 >= age_filter[1] & age_combine <= age_filter[2],
+           selected_var_date >= date_filter[1] & selected_var_date <= date_filter[2],
+           race_combine1 %in% race_filter,
+           sex_combine1 %in% gender_filter) %>%
+    group_by(selected_var_demo) %>% 
+    summarise(n = n()) %>% 
+    ungroup() %>% 
+    mutate(prop = n/sum(n)*100)
+  
+  plot_ly(pie_input.df,
+          type = "pie",
+          labels = ~selected_var_demo,
+          values = ~prop,
+          textinfo = "percent",
+          hoverinfo = "text",
+          text = ~paste("Number of individuals:",n),
+          marker = list(line = list(color = 'white', width = 1))) %>% 
+    layout(title = title_sel)
+  
+}
+  
 
 ui <- dashboardPage(
     skin = "blue",
@@ -18,41 +142,51 @@ ui <- dashboardPage(
         ),
         br(),
         dateRangeInput("date_filter", "Date range",
-                       min= "2022-01-15",
-                       max= Sys.Date(),
-                       start= "2022-01-15",
+                       min= "2021-12-01",
+                       max= end_date,
+                       start= "2021-12-01",
                        end= Sys.Date()),
         checkboxGroupInput("gender_filter", 
                            h4("Gender"), 
-                           choices = list("Male" = 1, 
-                                          "Female" = 2, 
-                                          "Other" = 3,
-                                          "Not disclosed" = 4),
-                           selected = c(1,2,3)),
+                           choices = list("Male", 
+                                          "Female", 
+                                          "Other",
+                                          "Unknown/ not disclosed"),
+                           selected = c("Male", 
+                                        "Female", 
+                                        "Other",
+                                        "Unknown/ not disclosed")),
         sliderInput("age_filter",
                     h4("Age"), 
                     min = 20, max = 100, value = c(20, 100)),
         checkboxGroupInput("race_filter", 
                            h4("Race/Ethnicity"), 
-                           choices = list("White/ Caucasian" = 1, 
-                                          "Black/ African American" = 2, 
-                                          "Hispanic/ Latino" = 3,
-                                          "Asian" = 4,
-                                          "Alaskan Native" = 5,
-                                          "Native American" = 6,
-                                          "Native Hawaiian and Other Pacific Islander"=7,
-                                          "Two or More Races" = 8,
-                                          "Unknown/ not disclosed" = 9),
-                           selected = seq(1,9))
+                           choices = list("White", 
+                                          "Black/ African American", 
+                                          "Hispanic/ Latino",
+                                          "Asian",
+                                          "Alaskan Native",
+                                          "Native American",
+                                          "Native Hawaiian and Other Pacific Islander",
+                                          "Multiracial/Multicultural",
+                                          "Unknown/ not disclosed"),
+                           selected = c("White", 
+                                          "Black/ African American", 
+                                          "Hispanic/ Latino",
+                                          "Asian",
+                                          "Alaskan Native",
+                                          "Native American",
+                                          "Native Hawaiian and Other Pacific Islander",
+                                          "Multiracial/Multicultural",
+                                          "Unknown/ not disclosed"))
     ),
     dashboardBody(
         style = "background-color:#2C3E50;",
         tabItem(tabName = "dashboard",
                 fluidRow(
-                    column(12, offset=1,
+                    column(12, offset=2,
                     valueBoxOutput("box_signup", width=2),
                     valueBoxOutput("box_contact", width=2),
-                    valueBoxOutput("box_consentdoc", width=2),
                     valueBoxOutput("box_app_scheduled", width=2),
                     valueBoxOutput("box_onboard", width = 2)
                     )
@@ -73,9 +207,19 @@ ui <- dashboardPage(
                                                          selected = "Value", multiple= F)
                                      ),
                                      column(width=12,
-                                            plotOutput("plot1", height = 250)
+                                            plotlyOutput("sign_up_bar", height = 250)
                                      )),
-                            tabPanel("Demographics")
+                            tabPanel("Demographics",
+                                     br(),
+                                     column(width = 6,
+                                            plotlyOutput("pie_race1", height = 250)
+                                     ),
+                                     column(width = 6,
+                                            plotlyOutput("pie_gender1", height = 250)
+                                     ),
+                                     column(width = 6,
+                                            plotlyOutput("pie_employee1", height = 250)
+                                     ))
                         )),
                     box(title = "Contact", solidHeader = F,  status = "primary",width=6,
                         tabsetPanel(
@@ -92,14 +236,33 @@ ui <- dashboardPage(
                                                          selected = "Value", multiple= F)
                                      ),
                                     column(width=12,
-                                    plotOutput("plot2", height = 250)
+                                    plotlyOutput("contact_bar", height = 250)
                                     )),
                             tabPanel("Call attempts stats"),
                             tabPanel("Eligible/ Interested stats")
                         )),
-                    box(title = "E-Mail sent", solidHeader = F,  status = "primary",width=6,
+                    # box(title = "E-Mail sent", solidHeader = F,  status = "primary",width=6,
+                    #     tabsetPanel(
+                    #         tabPanel("History",
+                    #                  br(),
+                    #                  column(width=4,
+                    #                         buttonGroup( inputId = "date_type3",
+                    #                                      choices = c("Cumulative", "Per day", "Per week"), 
+                    #                                      selected = "Cumulative", multiple= F)
+                    #                  ),
+                    #                  column(width=6,
+                    #                         buttonGroup( inputId = "value_type3",
+                    #                                      choices = c("Value", "Log"), 
+                    #                                      selected = "Value", multiple= F)
+                    #                  ),
+                    #                  column(width=12,
+                    #                         plotOutput("plot3", height = 250)
+                    #                  )),
+                    #         tabPanel("E-Mail stats")
+                    #     )),
+                    box(title = "Appointment", solidHeader = F,  status = "primary",width=6,
                         tabsetPanel(
-                            tabPanel("History",
+                            tabPanel("Appointment call",
                                      br(),
                                      column(width=4,
                                             buttonGroup( inputId = "date_type3",
@@ -112,13 +275,9 @@ ui <- dashboardPage(
                                                          selected = "Value", multiple= F)
                                      ),
                                      column(width=12,
-                                            plotOutput("plot3", height = 250)
+                                            plotlyOutput("appointment_bar", height = 250)
                                      )),
-                            tabPanel("E-Mail stats")
-                        )),
-                    box(title = "Appointment", solidHeader = F,  status = "primary",width=6,
-                        tabsetPanel(
-                            tabPanel("Appointment call",
+                            tabPanel("Visit scheduled",
                                      br(),
                                      column(width=4,
                                             buttonGroup( inputId = "date_type4",
@@ -127,23 +286,11 @@ ui <- dashboardPage(
                                      ),
                                      column(width=6,
                                             buttonGroup( inputId = "value_type4",
-                                                         choices = c("Value", "Log"), 
-                                                         selected = "Value", multiple= F)
+                                                        choices = c("Value", "Log"), 
+                                                        selected = "Value", multiple= F)
                                      ),
                                      column(width=12,
-                                            plotOutput("plot4", height = 250)
-                                     )),
-                            tabPanel("Visit scheduled",
-                                     br(),
-                                     column(width=4,
-                                            buttonGroup( inputId = "date_type5",
-                                                         choices = c("Cumulative", "Per day", "Per week"), 
-                                                         selected = "Cumulative", multiple= F),
-                                            column(width=6,
-                                                   buttonGroup( inputId = "value_type5",
-                                                                choices = c("Value", "Log"), 
-                                                                selected = "Value", multiple= F)
-                                            )
+                                           plotlyOutput("visit_bar", height = 250)
                                      ))
                         )),
                     box(title = "Onboarded", solidHeader = T,  status = "success", width=12,
@@ -179,49 +326,135 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
     
+  observeEvent(input$gender_filter,{
+    #
+      print(input$gender_filter)
+    print(input$race_filter)
 
-    
+
+  })
+
     output$box_signup <- renderValueBox({
+      
+
+      
         valueBox(
-            500, paste0("Sign up (today: ", 10,")"), icon = icon("sign-in"),
+          #assuming that all individuals in this file signed up 
+          nrow(data), paste0("Sign up (today: ", data_mod.df %>% 
+                                filter(sign_up_datetime == current_day) %>% 
+                                nrow(.),")"), icon = icon("sign-in"),
             color = "aqua"
         )
     })
     
     output$box_contact <- renderValueBox({
+      
+      value_input <- data_mod.df %>% 
+        filter(!is.na(initial_contact_response)) %>% 
+        nrow(.)
+      
         valueBox(
-            300, paste0("Contacted (today: ", 5,")"), icon = icon("phone"),
+          value_input, paste0("Contacted (today: ", data_mod.df %>% 
+                                filter(initial_contact_date == current_day) %>% 
+                                nrow(.),")"), icon = icon("phone"),
             color = "light-blue"
         )
     })
     
-    output$box_consentdoc <- renderValueBox({
-        valueBox(
-            200, paste0("Consent doc sent (today: ", 3,")"), icon = icon("file-alt"),
-            color = "blue"
-        )
-    })
-    
     output$box_app_scheduled <- renderValueBox({
+      
+      value_input <- data_mod.df %>% 
+                  filter(step %in% c("first _isit","appointment_scheduled")) %>% 
+                  nrow(.)
+      
         valueBox(
-            100, paste0("Appointment scheduled (today: ", 2,")"), icon = icon("phone"),
+          value_input, paste0("Appointment scheduled (today: ", data_mod.df %>% 
+                                filter(appointment_call_date == current_day) %>% 
+                                nrow(.),")"), icon = icon("phone"),
             color = "navy"
         )
     })
     
     output$box_onboard <- renderValueBox({
+      
+      #not sure if this is the right way to get this number 
+      value_input <- data_mod.df %>% 
+        filter(step %in% c("first_visit")) %>% 
+        nrow(.)
+      
         valueBox(
-            20, paste0("Onboarded (today: ", 10,")"),icon = icon("users"),
+          value_input, paste0("Onboarded (today: ", data_mod.df %>% 
+                                filter(visit_schedule_date == current_day) %>% 
+                                nrow(.),")"),icon = icon("users"),
             color = "green"
         )
     })
     
     
-    ### dummy plots
     
-    output$plot1 <- renderPlot({
-        hist(rnorm(500))
+    
+    #plot changes opon data_type selection 
+    
+    observeEvent(input$date_type1,{
+      
+      
+      output$sign_up_bar <- renderPlotly({
+            overviewplot_1("sign_up_datetime","Number of Sign-ups",input$date_type1,
+                           input$age_filter,input$date_filter,input$race_filter,input$gender_filter)
+
+          })
     })
+    
+    
+    observeEvent(input$date_type2,{
+      
+      output$contact_bar <- renderPlotly({
+        overviewplot_1("initial_contact_date","Number of Sign-ups",input$date_type2,
+                       input$age_filter,input$date_filter,input$race_filter,input$gender_filter)
+        
+      })
+      
+    })
+    
+    
+    observeEvent(input$date_type3,{
+      
+      output$appointment_bar <- renderPlotly({
+        overviewplot_1("appointment_call_date","Number of Sign-ups",input$date_type3,
+                       input$age_filter,input$date_filter,input$race_filter,input$gender_filter)
+        
+      })
+    })
+    
+    observeEvent(input$date_type4,{
+      
+      output$visit_bar <- renderPlotly({
+        overviewplot_1("visit_schedule_date","Number of Sign-ups",input$date_type4,
+                       input$age_filter,input$date_filter,input$race_filter,input$gender_filter)
+        
+      })
+    })
+    
+    
+    #Demographics of sign up -> could be added the same way also for the other steps of recruitment 
+    output$pie_race1 <- renderPlotly({
+      
+      demographics_pie("race_combine","sign_up_datetime","Race",input$age_filter,input$date_filter,input$race_filter,input$gender_filter)
+      
+    })
+    
+    output$pie_gender1 <- renderPlotly({
+      
+      demographics_pie("sex_combine","sign_up_datetime","Sex",input$age_filter,input$date_filter,input$race_filter,input$gender_filter)
+      
+    })
+    
+    output$pie_employee1 <- renderPlotly({
+      
+      demographics_pie("employ_status_desc","sign_up_datetime","Employee status",input$age_filter,input$date_filter,input$race_filter,input$gender_filter)
+      
+    })
+    
     output$plot2 <- renderPlot({
         hist(rnorm(500))
     })
